d78ff07447865767f3bd2f189e2e994c6733d21e
nakoibes <alefanow.d@yandex.ru>
2021-04-04 20:48:14 +0300

    ss

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 app.config.from_object(Config)
+migrate = Migrate(app, db)
 
-from app import routes, forms
+from app import routes, forms, models
--- config.py
+++ config.py
@@ -8,6 +8,5 @@ load_dotenv(dotenv_path)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
-#print(app.config)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
