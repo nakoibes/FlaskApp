784f9c7c50692c683f9ccfb060eedb797d593dca
nakoibes <alefanow.d@yandex.ru>
2021-04-06 02:35:27 +0300

    hg

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import Config
 
 app = Flask(__name__)
+login = LoginManager(app)
 db = SQLAlchemy(app)
 app.config.from_object(Config)
 migrate = Migrate(app, db)
+login.login_view = 'login'
 
 from app import routes, forms, models
